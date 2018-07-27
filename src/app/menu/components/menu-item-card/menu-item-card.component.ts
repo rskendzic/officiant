168287a5dbee883b039a435840f5d81f2b918810
Radovan Skendzic <rskendzic@execom.eu>
2018-07-27 09:54:57 +0200

    Extract item cards into components

---
+++ src/app/menu/components/menu-item-card/menu-item-card.component.ts
@@ -0,0 +1,29 @@
+import { Drink } from './../../models/Drink';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-item-card',
+  templateUrl: './menu-item-card.component.html',
+  styleUrls: ['./menu-item-card.component.scss']
+})
+export class MenuItemCardComponent implements OnInit {
+
+  @Input() drink: Drink
+  @Output() onUpdate = new EventEmitter<Drink>()
+  @Output() onDelete = new EventEmitter<Drink>()
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  updateDrink(){
+    this.onUpdate.emit()
+  }
+
+  deleteDrink(){
+    this.onDelete.emit()
+  }
+
+
+}
